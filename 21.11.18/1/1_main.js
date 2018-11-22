e7e28d3b7ead424d4d73edadf15e70c567a592a4
Dasha <rodionova_dashka@mail.ru>
2018-11-22 22:40:43 +0300

    Переделала

--- 21.11.18/1/1_main.js
+++ 21.11.18/1/1_main.js
@@ -1,16 +1,19 @@
 var range = (value_1,value_2, step) => { 
-    if (step == null)
+    if (step == null){
         step = 1;
-
-    var arr = []; 
-    
+    }
+    var arr =[]; 
     if (step > 0){
-        for (; value_1 < (value_2+1); value_1 += step) 
-        arr.push(value_1);
+        while (Math.abs(value_1) < Math.abs(value_2+1)){
+            arr.push(value_1);
+            value_1 += step;
+        } 
     }
     else{
-        for (; value_1 > (value_2-1); value_1 += step) 
-        arr.push(value_1);
+        while (Math.abs(value_1) > Math.abs(value_2-1)){
+            arr.push(value_1);
+            value_1 += step;
+        }
     }
     return arr; 
 } 
